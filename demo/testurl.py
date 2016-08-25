cad2c5e5fbbcb435a8950d2bad72adebf260371f
misasagi1988 <meng_yujing@yeah.net>
2016-08-25 18:58:14 +0800

    try get method

--- demo/testurl.py
+++ demo/testurl.py
@@ -1,4 +1,13 @@
 import os
+import urllib
 import urllib2
-response = urllib2.urlopen("http://www.baidu.com")
-print response.read()
+values = {}
+values["username"] = "meng_yujing@yeah.net"
+values["password"] = "Misasagi1988"
+data = urllib.urlencode(values)
+url = "http://passport.csdn.net/account/login"
+geturl = url + "?" + data
+print geturl
+#request = urllib2.Request("http://www.baidu.com")
+#response = urllib2.urlopen(request)
+#print response.read()
