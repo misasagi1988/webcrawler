1f25759d550664c2b4a54c042cbd73768f7ceea8
misasagi1988 <meng_yujing@yeah.net>
2016-08-26 10:16:12 +0800

    import cookielib

--- demo/testurl.py
+++ demo/testurl.py
@@ -1,13 +1,11 @@
 import os
 import urllib
 import urllib2
-values = {}
-values["username"] = "meng_yujing@yeah.net"
-values["password"] = "Misasagi1988"
-data = urllib.urlencode(values)
-url = "http://passport.csdn.net/account/login"
-geturl = url + "?" + data
-print geturl
-#request = urllib2.Request("http://www.baidu.com")
-#response = urllib2.urlopen(request)
-#print response.read()
+import cookielib
+cookie = cookielib.CookieJar()
+httphandler = urllib2.HTTPCookieProcessor(cookie)
+opener = urllib2.build_opener(httphandler)
+response = opener.open("http://www.baidu.com")
+for item in cookie:
+    print "Name: ", item.name
+    print "Value: ", item.value
